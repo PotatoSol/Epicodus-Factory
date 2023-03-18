c7ccf9c7ebf23cae10186f7f7c0f2a0ec8317cc1
Michael Sol <sol.mike.j@gmail.com>
2023-03-17 18:45:00 -0700

    more naming changes

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,13 +6,8 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
-
     [Required(ErrorMessage = "The Engineer's name can't be empty!")]
     public string Name { get; set; }
-    
-    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
-    public int CategoryId { get; set; }
-    public Category Category { get; set; }
-    public List<ItemTag> JoinEntities { get;}
+    public List<EngineerMachine> JoinEntities { get;}
   }
 }
